c7b98cc3aad83f8232a4d3eecd7830a71b541d17
ozgeyamc <ozgee.yamacc@gmail.com>
2026-08-07 18:12:07 +0300

    Add UcretSatiri dataclass to models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class UcretSatiri:
+    kategori: str
+    masraf: str
+    asgari_tutar: str = ""
+    asgari_oran: str = ""
+    azami_tutar: str = ""
+    azami_oran: str = ""
+    aciklama: str = ""
+    site_guncelleme_tarihi: str = ""
+    kanal: str = ""  # "mobil", "sube", ""
